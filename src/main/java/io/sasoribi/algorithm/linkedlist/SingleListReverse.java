364da55a68240dc4e63b551d987ee0294f1d2660
LAPTOP-O5281FOC\shaojiacheng <shaojiacheng@intasect.com.cn>
2021-08-13 13:59:59 +0800

    单链表翻转

---
+++ src/main/java/io/sasoribi/algorithm/linkedlist/SingleListReverse.java
@@ -0,0 +1,46 @@
+package io.sasoribi.algorithm.linkedlist;
+
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class SingleListReverse {
+    // 迭代---Success
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+        ListNode next;
+        while(current != null){
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;
+    }
+    
+    static class ListNode {
+        int val;
+        ListNode next;
+        
+        ListNode() {
+        }
+        
+        ListNode(int val) {
+            this.val = val;
+        }
+        
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}

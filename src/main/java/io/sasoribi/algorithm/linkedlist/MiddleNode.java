03723552f23c9247d3b66edf4dd862db9f667f63
LAPTOP-O5281FOC\shaojiacheng <shaojiacheng@intasect.com.cn>
2021-08-19 13:26:16 +0800

    快慢指针找链表中点;

---
+++ src/main/java/io/sasoribi/algorithm/linkedlist/MiddleNode.java
@@ -0,0 +1,36 @@
+package io.sasoribi.algorithm.linkedlist;
+
+/**
+ * Given the head of a singly linked list, return the middle node of the linked list.
+ * <p>
+ * If there are two middle nodes, return the second middle node.
+ */
+public class MiddleNode {
+    
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            head = head.next;
+        }
+        return head;
+    }
+    
+    static class ListNode {
+        int val;
+        ListNode next;
+        
+        ListNode() {
+        }
+        
+        ListNode(int val) {
+            this.val = val;
+        }
+        
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    
+}
--- src/main/java/io/sasoribi/algorithm/linkedlist/RemoveNNodeFromEnd.java
+++ src/main/java/io/sasoribi/algorithm/linkedlist/RemoveNNodeFromEnd.java
@@ -1,17 +1,11 @@
 package io.sasoribi.algorithm.linkedlist;
 
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode() {}
- * ListNode(int val) { this.val = val; }
- * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
+ *     #19
+ *     Given the head of a linked list,
+ *     remove the nth node from the end of the list and return its head.
  */
 public class RemoveNNodeFromEnd {
-    //#19 Given the head of a linked list, remove the nth node from the end of the list and return its head.
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode fast = new ListNode(0,head);
         ListNode slow = head = fast;

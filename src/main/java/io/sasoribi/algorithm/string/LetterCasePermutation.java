b759bbad6f5019bb6cd2b7d1e297f2e27a9c1038
LAPTOP-O5281FOC\shaojiacheng <shaojiacheng@intasect.com.cn>
2021-08-21 15:24:14 +0800

    字母大小写全排列;

---
+++ src/main/java/io/sasoribi/algorithm/string/LetterCasePermutation.java
@@ -0,0 +1,42 @@
+package io.sasoribi.algorithm.string;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given a string s,
+ * we can transform every letter individually to be lowercase or uppercase to create another string.
+ *
+ * Return a list of all possible strings we could create. You can return the output in any order.
+ *
+ * Example 1:
+ *
+ * Input: s = "a1b2"
+ * Output: ["a1b2","a1B2","A1b2","A1B2"]
+ * Example 2:
+ *
+ * Input: s = "3z4"
+ * Output: ["3z4","3Z4"]
+ * Example 3:
+ *
+ * Input: s = "12345"
+ * Output: ["12345"]
+ * Example 4:
+ *
+ * Input: s = "0"
+ * Output: ["0"]
+ *
+ * Constraints:
+ *
+ * s will be a string with length between 1 and 12.
+ * s will consist only of letters or digits.
+ *
+ */
+public class LetterCasePermutation {
+    public List<String> letterCasePermutation(String s) {
+        List<String> rs = new ArrayList<>();
+    
+        return rs;
+    }
+    
+}

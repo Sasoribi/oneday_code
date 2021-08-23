9f441ff24ec8211d11b440bc03af043ed7c291ff
LAPTOP-O5281FOC\shaojiacheng <shaojiacheng@intasect.com.cn>
2021-08-23 16:06:11 +0800

    二叉树最大深度;BFS;

--- src/main/java/io/sasoribi/algorithm/binarytree/MaxDepthOfTree.java
+++ src/main/java/io/sasoribi/algorithm/binarytree/MaxDepthOfTree.java
@@ -1,32 +1,35 @@
 package io.sasoribi.algorithm.binarytree;
 
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Given the root of a binary tree, return its maximum depth.
- *
+ * <p>
  * A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
- *
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: root = [3,9,20,null,null,15,7]
  * Output: 3
  * Example 2:
- *
+ * <p>
  * Input: root = [1,null,2]
  * Output: 2
  * Example 3:
- *
+ * <p>
  * Input: root = []
  * Output: 0
  * Example 4:
- *
+ * <p>
  * Input: root = [0]
  * Output: 1
- *
+ * <p>
  * Constraints:
- *
+ * <p>
  * The number of nodes in the tree is in the range [0, 104].
  * -100 <= Node.val <= 100
- *
  */
 public class MaxDepthOfTree {
     
@@ -45,11 +48,41 @@ public class MaxDepthOfTree {
      * }
      * }
      */
+    public int iterationDepth(TreeNode root) {
+        int depth = 0;
+        if(root == null){
+            return depth;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        int currentLay = 0;
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            currentLay = queue.size();
+            while (currentLay > 0) {
+                TreeNode node = queue.poll();
+                
+                if (node.left != null)
+                    queue.offer(node.left);
+                if (node.right != null)
+                    queue.offer(node.right);
+                currentLay--;
+            }
+            depth++;
+        }
+        
+        return depth;
+    }
+    
+    
+    //Recursive
     public int maxDepth(TreeNode root) {
         return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
     }
     
     //->
+    
     public int maxDepthE(TreeNode root) {
         //到达叶子
         if (root == null) {

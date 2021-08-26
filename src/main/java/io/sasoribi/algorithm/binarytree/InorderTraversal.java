6b05ec0d5fcdbb98f708c507c2b0cc2e7ffdb114
LAPTOP-O5281FOC\shaojiacheng <shaojiacheng@intasect.com.cn>
2021-08-26 09:01:36 +0800

    迭代中序遍历二叉树;

--- src/main/java/io/sasoribi/algorithm/binarytree/InorderTraversal.java
+++ src/main/java/io/sasoribi/algorithm/binarytree/InorderTraversal.java
@@ -1,8 +1,6 @@
 package io.sasoribi.algorithm.binarytree;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 /**
  * Given the root of a binary tree, return the inorder traversal of its nodes' values.
@@ -11,6 +9,29 @@ public class InorderTraversal {
     ArrayList<Integer> list = new ArrayList<>();
     
     //#94.Binary Tree Inorder Traversal
+    public List<Integer> inorderTraversal_iterate(TreeNode root) {
+        if (root == null) {
+            return list;
+        }
+        
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        
+        
+        while (root != null || !stack.isEmpty() ) {
+            while ( root !=null && (root = root.left) != null) {
+                stack.push(root);
+            }
+            //EndOfLeft
+            root = stack.pop();
+            list.add(root.val);
+            
+            root = root.right;
+        }
+        return list;
+    }
+    
+    // /->Recursive
     public List<Integer> inorderTraversal(TreeNode root) {
         if (root == null) {
             return Collections.emptyList();

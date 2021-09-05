74c7d364327ddc511359a07e8129805709f8f6fa
LAPTOP-O5281FOC\shaojiacheng <shaojiacheng@intasect.com.cn>
2021-09-05 14:49:53 +0800

    回溯法生成子集;

--- src/main/java/io/sasoribi/algorithm/calculate/Subsets.java
+++ src/main/java/io/sasoribi/algorithm/calculate/Subsets.java
@@ -6,29 +6,32 @@ import java.util.List;
 /**
  * Given an integer array nums of unique elements,
  * return all possible subsets (the power set).
- *
+ * <p>
  * The solution set must not contain duplicate subsets.
  * Return the solution in any order.
- *
  */
 public class Subsets {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Object> subset = new ArrayList<>();
-        boolean[] statusArr = new boolean[nums.length];
+        ans.add(new ArrayList<>());
+        if (nums.length == 0) {
+            return ans;
+        }
+        List<Integer> subset = new ArrayList<>();
+        recursive(nums, ans, subset, 0);
         
-        recursive(nums,ans,subset,statusArr);
-        
-
+        return ans;
         
     }
     
-    private void recursive(int[] nums,List<List<Integer>> ans, List<Object> subset, boolean[] statusArr) {
-    
-        for (int i = 0; i < nums.length; i++) {
+    private void recursive(int[] nums, List<List<Integer>> ans, List<Integer> subset, int index) {
+        
+        // 第一次循环记录含有i=0,第二次循环不含i=0的元素;
+        for (int i = index; i < nums.length; i++) {
             subset.add(nums[i]);
-            
-            statusArr[i] = true;
+            ans.add(new ArrayList<>(subset));
+            recursive(nums, ans, subset, i + 1);
+            subset.remove(subset.size() - 1);
         }
     }
 }

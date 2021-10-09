c8e3aa2ab9956611ed12e55a1aa16f74acfebd87
LAPTOP-O5281FOC\shaojiacheng <shaojiacheng@intasect.com.cn>
2021-10-09 15:01:35 +0800

    二叉树最深公共祖先

---
+++ src/main/java/io/sasoribi/algorithm/binarytree/LowestCommonAncestorOfABinaryTree.java
@@ -0,0 +1,28 @@
+package io.sasoribi.algorithm.binarytree;
+
+/**
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ * <p>
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个节点 p、q，
+ * 最近公共祖先表示为一个节点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ */
+public class LowestCommonAncestorOfABinaryTree {
+    //236. 二叉树的最近公共祖先
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q) {
+            return root;
+        }
+        if (root != null) {
+            TreeNode lNode = lowestCommonAncestor(root.left, p, q);
+            TreeNode rNode = lowestCommonAncestor(root.right, p, q);
+            if (lNode != null && rNode != null)
+                return root;
+            else if (lNode == null) //两个都在右子树
+                return rNode;
+            else //两个都在左子树里面
+                return lNode;
+            
+        }
+        return null;
+    }
+}
